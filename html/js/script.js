2ea8866efeb0b0bd69e37cbe894ebfffb0665cff
jiocb333 <jiocb333777@gmail.com>
2018-10-25 21:55:27 +0300

    first

---
+++ html/js/script.js
@@ -0,0 +1,22 @@
+let a=1;
+if (a==1){alert('hello')}else{alert('fuck')}
+let m;
+m=prompt('input hv:');
+if ( m>=0 && m<=30 ) { console.log('first hour'); }
+else if (m>30 && m<=60){console.log('second hour');}
+/*-------------------------------*/
+let n=3;
+if (n==0) {
+  console.log('sory pysto)');
+}
+console.log(n==0? 'pysto':'not');
+/*----------------------------*/
+if (n>=2 && n<=5 ){console.log('2-5 aples');}
+else {
+  console.log('no ');
+}
+/*------------------------*/
+if (n%3===0 || n%7==0){console.log('zaebis');}
+else {
+  console.log('bead');
+}
